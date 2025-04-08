b84988e5d5d562717501e7921d77a3a57b7badfd
Nellutla123 <nikhilnellutla100@gmail.com>
2025-04-08 12:40:23 +0530

    data evaluation and Mlflow completed

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -27,6 +27,7 @@ from sklearn.ensemble import (
 
 
 
+
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
         try:
