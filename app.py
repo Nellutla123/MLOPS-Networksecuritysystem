f914cbef7277d9170aa3ae922d6ae5115290d244
Nellutla123 <nikhilnellutla100@gmail.com>
2025-04-11 14:37:48 +0530

    final deployment

--- app.py
+++ app.py
@@ -77,6 +77,7 @@ async def predict_route(request:Request,file:UploadFile=File(...)):
         table_html = df.to_html(classes = 'table table-striped')
         
         return templates.TemplateResponse("table.html",{"request": request,"table":table_html})
+    
     except Exception as e:
         raise NetworkSecurityException(e,sys)
     
